b449d62a4fbc14ee3af1359fb37b31f09b149b02
narjiss <narjiss.elmekadem@gmail.com>
2025-05-21 01:51:08 +0100

    delet all integrate file

--- src/__mocks__/server.ts
+++
@@ -1,7 +0,0 @@
-// src/__mocks__/server.ts
-import { handlers } from './handlers';
-
-export const server = (async () => {
-  const { setupServer } = await import('msw/node');
-  return setupServer(...handlers);
-})();
