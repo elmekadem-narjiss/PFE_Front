31539bde3335562d68fd4cba78f16c378757e722
elmekadem-narjiss <elaouicha@gmail.com>
2025-06-14 13:22:18 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,1 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: 'export', // Enables static export
-  trailingSlash: true, // Optional: Adds trailing slashes to URLs for compatibility with S3
-};
 
-module.exports = nextConfig;
